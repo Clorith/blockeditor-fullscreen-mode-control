3a18672c36da2f7163f88aedb71b551f39646ee9
Marius Jensen <marius@clorith.net>
2021-10-03 15:49:27 +0200

    Add a donate link to the plugin

--- includes/class-cbfm-block-editor.php
+++ includes/class-cbfm-block-editor.php
@@ -2,10 +2,18 @@
 
 class CBFM_Block_Editor {
 
+	/**
+	 * Build the class events.
+	 */
 	public function __construct() {
 		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_script' ) );
+
+		add_filter( 'plugin_action_links', array( $this, 'add_donate_link' ), 10, 2 );
 	}
 
+	/**
+	 * Enqueue the JavaScript used to maintain editor settings across sessions.
+	 */
 	function enqueue_script() {
 		wp_enqueue_script(
 			'cbfm-manager',
@@ -29,6 +37,29 @@ class CBFM_Block_Editor {
 			'cbfm_manager',
 			$settings
 		);
+	}
+
+	/**
+	 * Add a donate link to the row actions for this plugin.
+	 *
+	 * @param array  $actions An array of plugin actions.
+	 * @param string $file    The string identifying the primary plugin file.
+	 *
+	 * @return array
+	 */
+	public function add_donate_link( $actions, $file ) {
+		// Return early if this is not the entry data for this plugin.
+		if ( 'blockeditor-fullscreen-mode-control/blockeditor-fullscreen-mode-control.php' !== $file ) {
+			return $actions;
+		}
+
+		$actions['cbfm-donate'] = sprintf(
+			'<a href="%s" target="_blank"><span style="font-weight:600">%s</span></a>',
+			esc_url( 'https://www.paypal.me/clorith' ),
+			esc_html__( 'Donate to this plugin', 'blockeditor-fullscreen-mode-control' )
+		);
+
+		return $actions;
 	}
 
 }
